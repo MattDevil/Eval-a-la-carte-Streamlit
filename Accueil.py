029a70e5b6c1a44f32256ec5c8e5b34ab36caf9c
Matthieu Devillers <matthieu.devillers@ac-rennes.fr>
2023-01-02 00:21:06 +0100

    Pdf

--- Accueil.py
+++ Accueil.py
@@ -298,7 +298,7 @@ with tab6:
             #os.startfile(nom_fichier_eval+".pdf")
             subprocess.call(['pdflatex',nom_fichier_eval])
             nom_fichier_pdf = nom_fichier_eval[:-3]+"pdf"
-            st.write(nom_fichier_pdf)
+            print(nom_fichier_pdf)
             with open(nom_fichier_eval, 'rb') as f:
                 if st.download_button('Téléchargez le fichier .tex', f, file_name=nom_fichier_eval):
                     f.close()
