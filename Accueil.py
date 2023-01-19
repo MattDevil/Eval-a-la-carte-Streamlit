c996b9de8e4387714512848765e927041632da66
MattDev <matthieu.devillers@ac-rennes.fr>
2023-01-19 21:06:16 +0100

    MAJ

--- Accueil.py
+++ Accueil.py
@@ -302,7 +302,7 @@ with tab6:
         # st.write(nom_fichier_pdf)
         with open(nom_fichier_eval, 'rb') as f:
             if st.download_button('Téléchargez le fichier .tex', f, file_name=nom_fichier_eval) :
-                pass
+                f.close()
             
             if st.button("Effacer le fichier .tex"):
                 os.system("rm "+nom_fichier_eval)
