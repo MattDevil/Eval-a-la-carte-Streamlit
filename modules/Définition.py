3e1f4f3c849dabb2d37b9ea2bab5d0243b2c58d8
MattDev <matthieu.devillers@ac-rennes.fr>
2023-05-21 20:55:55 +0200

    Update Definition.py
    Ligne 49 : virer le sep

--- modules/Définition.py
+++ modules/Définition.py
@@ -46,7 +46,7 @@ def recupDemandes() :
 
     if fichierdemandes is not None and fichierdemandes.name[-4:] == ".csv" :
         st.success("Fichier bien reçu !")
-        dataframe_demandes = pd.read_csv(fichierdemandes, sep)
+        dataframe_demandes = pd.read_csv(fichierdemandes) # ou pd.read_csv(fichierdemandes, sep)
         st.write(dataframe_demandes)
         return dataframe_demandes
     else :
